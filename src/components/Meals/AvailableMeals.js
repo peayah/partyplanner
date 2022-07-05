6e7ad9cc37c1f8c648afe7dccacec62fe23f8fb9
peayah <82358003+peayah@users.noreply.github.com>
2022-07-05 13:44:43 -0700

    add meal item logic

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,4 +1,5 @@
 import Card from "../UI/Card"
+import MealItem from "./MealItem/MealItem"
 import classes from "./AvailableMeals.module.css"
 
 const DUMMY_MEALS = [
@@ -29,14 +30,23 @@ const DUMMY_MEALS = [
 ];
 
 const AvailableMeals = () => {
-    const mealsList = DUMMY_MEALS.map(meal => <li>{ meal.name }</li>);
-    return <section className={classes.meals}>
-        <Card>
-            <ul>
-                { mealsList } 
-            </ul>
-        </Card>  
-    </section>
-}
+    const mealsList = DUMMY_MEALS.map(meal => (
+    <MealItem 
+        key={meal.id} 
+        name={meal.name} 
+        description={meal.description} 
+        price={meal.price}
+        />
+    ));
+
+    return (
+        <section className={classes.meals}>
+            <Card>
+                <ul>
+                    { mealsList } 
+                </ul>
+            </Card>  
+        </section>
+)}
 
 export default AvailableMeals
