77e5484b81fe5ed029103cc55d600da8f7e4845f
peayah <82358003+peayah@users.noreply.github.com>
2022-07-06 16:40:49 -0700

    add add item functionality

--- src/store/cartProvider.js
+++ src/store/cartProvider.js
@@ -1,13 +1,47 @@
 import CartContext from "./cart-context";
+import { useReducer } from "react";
+
+const defaultCartState = {
+    items:[],
+    totalAmount: 0
+}
+
+const cartReducer = (state, action) => {
+    if (action.type === "ADD") {
+        const updatedItems = state.items.concat(action.item);
+        const updatedTotalAmount = state.totalAmount + action.item.price * action.item.amount;
+        return {
+            items: updatedItems,
+            totalAmount: updatedTotalAmount
+        }
+    }
+    if (action.type === "REMOVE") {
+
+    }
+
+    return defaultCartState
+}
 
 const CartProvider = props => {
 
-    const addItemToCartHandler = item => {};
-    const removeItemfromCartHandler = id => {};
+    const [CartState, dipatchCartAction] = useReducer(cartReducer, defaultCartState);
+
+    const addItemToCartHandler = (item) => {
+        dipatchCartAction({
+            type: 'ADD',
+            item: item
+        })
+    };
+    const removeItemfromCartHandler = (id) => {
+        dipatchCartAction({
+            type: 'REMOVE',
+            item: id
+        })
+    };
     
     const cartContext = {
-        items: [],
-        totalAmount: 0,
+        items: CartState.items,
+        totalAmount: CartState.totalAmount,
         addItem: addItemToCartHandler,
         removeItem: removeItemfromCartHandler
     };
