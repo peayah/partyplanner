ece1bce6ff53eed05e8f3c93df77a5f1c707607d
peayah <82358003+peayah@users.noreply.github.com>
2022-07-05 22:31:24 -0700

    add overlay functionality

--- src/components/UI/Modal.js
+++ src/components/UI/Modal.js
@@ -1,8 +1,29 @@
+import { Fragment } from "react";
 import classes from "./Modal.module.css"
+import { ReactPortal } from "react";
+
+
+const Backdrop = props => {
+    return <div className={classes.Backdrop}/>
+};
+
+const ModalOverlay = props => {
+
+    return (<div className={classes.Modal}>
+        <div className={classes.content}>
+            {props.children}
+        </div>
+    </div>
+    )
+}
+
+const portalElement = document.getElementById('overlays');
 
 const Modal = props => {
-
-
+    return <Fragment>
+        {ReactDOM.createPortal(<Backdrop/>, portalElement)},
+        {ReactDOM.createPortal(<ModalOverlay>(props.children)</ModalOverlay>, portalElement)}
+    </Fragment>
 }
 
 export default Modal;
