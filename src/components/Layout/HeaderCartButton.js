9588dc518ec013fcbcdd73960a9bd8a1f756ea0f
peayah <82358003+peayah@users.noreply.github.com>
2022-07-06 21:34:13 -0700

    add bump when first item added

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,26 +1,36 @@
 import CartIcon from "../Cart/CartIcon"
 import classes from "./HeaderCartButton.module.css"
-import { useContext } from "react"
+import { useContext, useEffect, useState } from "react"
 import CartContext from "../../store/cart-context"
 
 
 const HeaderCartButton = (props) => {
+    const [btnIsHighlighted, setBtnIsHighlighted] = useState(false);
     const cartCtx = useContext(CartContext)
-
-    const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    
+    const { items } = cartCtx;
+    
+    const numberOfCartItems = items.reduce((curNumber, item) => {
         return curNumber + item.amount;
     },0)
 
-    const btnClasses = `${classes.button} ${classes.bump}`;
+    const btnClasses = `${classes.button} ${btnIsHighlighted ? classes.bump : ""}`;
+
+    useEffect(() => {
+        if(items.length === 0) {
+            return;
+        }
+        setBtnIsHighlighted(true);
+    }, [items]);
 
     return (
-    <button className={btnClasses} onClick={props.onClick}>
-        <span className={classes.icon}>
-            <CartIcon/>
-        </span>
-        <span >Your Cart</span>
-        <span className={classes.badge}> {numberOfCartItems}</span>
-    </button>
-);
+        <button className={btnClasses} onClick={props.onClick}>
+            <span className={classes.icon}>
+                <CartIcon/>
+            </span>
+            <span >Your Cart</span>
+            <span className={classes.badge}> {numberOfCartItems}</span>
+        </button>
+    );
 }
 export default HeaderCartButton;
