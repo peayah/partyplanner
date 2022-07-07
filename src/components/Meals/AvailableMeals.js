1e99a05486719076376211b80e2e63321f60149b
peayah <82358003+peayah@users.noreply.github.com>
2022-07-07 13:30:46 -0700

    add load error handeling

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -6,10 +6,18 @@ import classes from "./AvailableMeals.module.css"
 const AvailableMeals = () => {
     const [meals, setMeals] = useState([]);
     const[isLoading, setIsLoading] = useState(true)
+    const[httpError, setHttpError] = useState()
 
     useEffect(() => {
         const fetchMeals = async () => {
-        const response = await fetch("https://partyplannerfoodorderapp-default-rtdb.firebaseio.com/mealitems.json");
+        const response = await fetch(
+            "https://partyplannerfoodorderapp-default-rtdb.firebaseio.com/mealitems.json"
+            );
+
+        if(!response.ok) {
+            throw new Error("Something went wrong!");
+        }    
+
         const responseData = await response.json();
 
         const loadedMeals = [];
@@ -25,7 +33,11 @@ const AvailableMeals = () => {
         setMeals(loadedMeals);
         setIsLoading(false);
         };
-    fetchMeals();
+    
+        fetchMeals().catch((error) => {
+        setIsLoading(false);
+        setHttpError(error.message);
+        });
     }, []);
 
     if (isLoading) {
@@ -33,6 +45,14 @@ const AvailableMeals = () => {
             <section className={classes.MealsLoading}>
                 <p>Loading ...</p>
             </section> 
+        )
+    }
+
+    if(httpError) {
+        return (
+            <section className={classes.MealLoading}>
+                <p>{httpError}</p>
+            </section>
         )
     }
     
