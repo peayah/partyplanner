b057891907f83fdb036e548a7441009bf4c14376
peayah <82358003+peayah@users.noreply.github.com>
2022-07-07 00:36:56 -0700

    update message

--- src/components/Meals/MealsSummary.js
+++ src/components/Meals/MealsSummary.js
@@ -6,13 +6,15 @@ const MealsSummary = () => {
     <section className={classes.summary}>
       <h2>Delicious Food, Delivered To You</h2>
       <p>
-        Choose your favorite meal from our broad selection of available meals
-        and enjoy a delicious lunch or dinner at home.
+        Choose from our broad selection of brunch, lunch or dinner meals.
+      </p>
+      <p>You can enjoy the company of your guests while we take care of your meal.
       </p>
       <p>
-        All our meals are cooked with high-quality ingredients, just-in-time and
-        of course by experienced chefs!
+        All our meals are cooked with high-quality ingredients, just-in-time by our experienced chefs!
       </p>
+      <p>All portions are of servings of four.</p>
+      <p>All prices include tax.</p>
     </section>
   );
 };
