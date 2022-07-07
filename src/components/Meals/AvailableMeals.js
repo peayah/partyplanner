4bd0be44cb125afa779553961a28b5a1abcb8af5
peayah <82358003+peayah@users.noreply.github.com>
2022-07-07 00:52:04 -0700

    add "loading" text

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -3,35 +3,9 @@ import Card from "../UI/Card"
 import MealItem from "./MealItem/MealItem"
 import classes from "./AvailableMeals.module.css"
 
-// const DUMMY_MEALS = [
-//     {
-//     id: 'm1',
-//     name: 'Sushi',
-//     description: 'Finest fish and veggies',
-//     price: 22.99,
-//     },
-//     {
-//     id: 'm2',
-//     name: 'Schnitzel',
-//     description: 'A german specialty!',
-//     price: 16.5,
-//     },
-//     {
-//     id: 'm3',
-//     name: 'Barbecue Burger',
-//     description: 'American, raw, meaty',
-//     price: 12.99,
-//     },
-//     {
-//     id: 'm4',
-//     name: 'Green Bowl',
-//     description: 'Healthy...and green...',
-//     price: 18.99,
-//     },
-// ];
-
 const AvailableMeals = () => {
     const [meals, setMeals] = useState([]);
+    const[isLoading, setIsLoading] = useState(true)
 
     useEffect(() => {
         const fetchMeals = async () => {
@@ -49,9 +23,18 @@ const AvailableMeals = () => {
             });
         }
         setMeals(loadedMeals);
+        setIsLoading(false);
         };
     fetchMeals();
     }, []);
+
+    if (isLoading) {
+        return (
+            <section className={classes.MealsLoading}>
+                <p>Loading ...</p>
+            </section> 
+        )
+    }
     
     const mealsList = meals.map(meal => (
     <MealItem 
