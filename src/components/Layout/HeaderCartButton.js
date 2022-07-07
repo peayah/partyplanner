8859c156dabde4b45275d8360945500ed6120490
peayah <82358003+peayah@users.noreply.github.com>
2022-07-06 21:01:03 -0700

    add button bump effect

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -6,15 +6,21 @@ import CartContext from "../../store/cart-context"
 
 const HeaderCartButton = (props) => {
     const cartCtx = useContext(CartContext)
+
     const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
         return curNumber + item.amount;
     },0)
 
-    return <button className={classes.button} onClick={props.onClick}>
-        <span className={classes.icon}><CartIcon/></span>
+    const btnClasses = `${classes.button} ${classes.bump}`;
+
+    return (
+    <button className={btnClasses} onClick={props.onClick}>
+        <span className={classes.icon}>
+            <CartIcon/>
+        </span>
         <span >Your Cart</span>
         <span className={classes.badge}> {numberOfCartItems}</span>
     </button>
+);
 }
-
 export default HeaderCartButton;
