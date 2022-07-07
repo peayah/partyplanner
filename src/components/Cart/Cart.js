e2b93f7a42da8ff34b04ff7ae297e5050824fa96
peayah <82358003+peayah@users.noreply.github.com>
2022-07-07 14:03:54 -0700

    add cancel button and functionality

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -53,7 +53,7 @@ const orderHandler =() => {
             <span>Total Amount</span>
             <span>{totalAmount}</span>
         </div>
-        {isCheckout && <Checkout/>}
+        {isCheckout && <Checkout onCancel={props.onClose}/>}
         {!isCheckout && modalActions}
         
     </Modal>  
