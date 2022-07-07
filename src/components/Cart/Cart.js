5439c09e822e8bb0adc5a2ce4c45a0a52303c3f9
peayah <82358003+peayah@users.noreply.github.com>
2022-07-06 17:37:20 -0700

    add total purchase functionality

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,15 @@
 import Modal from "../UI/Modal";
 import classes from "./Cart.module.css"
+import { useContext } from "react";
+import CartContext from "../../store/cart-context"
 
 const Cart =(props) => {
+
+    const cartCtx = useContext(CartContext);
+
+    const totalAmount  = `$${cartCtx.totalAmount.toFixed(2)}`;
+    const hasItems = cartCtx.items.length > 0;
+
     const cartItems = (
         <ul className={classes["cart-items"]}> 
         {
@@ -23,11 +31,11 @@ const Cart =(props) => {
         {cartItems}
         <div className={classes.total}>
             <span>Total Amount</span>
-            <span>35.62</span>
+            <span>{totalAmount}</span>
         </div>
         <div className={classes.actions}>
             <button className={classes["button--alt"]} onClick={props.onClose}>Close</button>
-            <button className={classes.button}>Order</button>
+            {hasItems && <button className={classes.button}>Order</button>}
         </div>
     </Modal>  
     );
