13bd9297e39748a2a0327d3981fa07a027e240a0
peayah <82358003+peayah@users.noreply.github.com>
2022-07-27 19:22:58 -0700

    Updates

---
+++ precache-manifest.cf1304d1dee5fa1f4e41a657d61ef6c6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "e94979674d2a4f9260ef",
+    "url": "/partyplanner/static/css/main.838d5de9.chunk.css"
+  },
+  {
+    "revision": "e94979674d2a4f9260ef",
+    "url": "/partyplanner/static/js/main.e9497967.chunk.js"
+  },
+  {
+    "revision": "fad06857e6d4f38d12bf",
+    "url": "/partyplanner/static/js/1.fad06857.chunk.js"
+  },
+  {
+    "revision": "1faa9bde3ed1cc937c11",
+    "url": "/partyplanner/static/js/runtime~main.1faa9bde.js"
+  },
+  {
+    "revision": "55c749900b91566d7fa1eea3937e0e04",
+    "url": "/partyplanner/static/media/food.55c74990.jpeg"
+  },
+  {
+    "revision": "8d20f77a066a3d65722a38816d8be68a",
+    "url": "/partyplanner/index.html"
+  }
+];
